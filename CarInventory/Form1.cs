8c8b9bcef8866fb891725ca185f4261511f7c8eb
Tony Theodoropoulos <theodot@outlook.com>
2019-04-01 16:30:10 -0400

    Add project files.

---
+++ CarInventory/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarInventory
+{
+    public partial class Form1 : Form
+    {
+        List<Car> inventory = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string year, make, colour, mileage;
+
+            year = yearInput.Text;
+            make = makeInput.Text;
+            colour = colourInput.Text;
+            mileage = mileageInput.Text;
+
+            Car c = new Car(year, make, colour, mileage);
+            inventory.Add(c);
+
+            outputLabel.Text = yearInput.Text = makeInput.Text = colourInput.Text = mileageInput.Text = "";
+            yearInput.Focus();
+            displayItems();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                if (inventory[i].make == makeInput.Text)
+                {
+                    inventory.RemoveAt(i);
+                }
+            }
+
+            int index = inventory.FindIndex(car => car.make == makeInput.Text);
+
+            if (index >-1)
+            {
+                inventory.RemoveAt(index);
+            }
+
+            displayItems();
+        }
+
+        public void displayItems()
+        {
+            outputLabel.Text = "";
+
+            foreach (Car c in inventory)
+            {
+                outputLabel.Text += c.year + " "
+                     + c.make + " "
+                     + c.colour + " "
+                     + c.mileage + "\n";
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
